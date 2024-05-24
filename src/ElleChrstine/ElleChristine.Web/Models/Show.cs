75e9606fc8155962b45bb4ed7bc39ec00e334973
RedBirdOBX <shane.fowlkes.70@gmail.com>
2024-05-24 10:18:56 -0400

    wip

--- src/ElleChrstine/ElleChristine.Web/Models/Show.cs
+++ src/ElleChrstine/ElleChristine.Web/Models/Show.cs
@@ -4,11 +4,18 @@
     {
         public Show()
         {
+            Title = string.Empty;
             Location = string.Empty;
             Time = string.Empty;
             Description = string.Empty;
             Image = string.Empty;
+            Url = string.Empty;
+            MapUrl = string.Empty;
         }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
 
         public string Location { get; set; }
 
@@ -19,5 +26,9 @@
         public string Description { get; set; }
 
         public string Image { get; set; }
+
+        public string Url { get; set; }
+
+        public string MapUrl { get; set; }
     }
 }
